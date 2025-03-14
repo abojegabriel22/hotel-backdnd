a951e6ff9c454e8a4734e880d3d5b06a8b9225a9
aboje gabriel <abojejunior@gmail.com>
2025-03-14 23:13:21 +0100

    adding port to env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT
 const dbConfig = require("./db")
 const dbc = require("./db")
 const roomsRoute = require("./routes/roomsRoute")
